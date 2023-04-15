9b7d0f0533f8ad132014909fc2edd5e3b915b374
Vince F <Vince F>
2023-04-15 12:19:03 +0200

    added base tests for normal item

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,12 +1,69 @@
-
 from gilded_rose import Item, GildedRose
 
 
-class TestGildedRose:
-    def test_foo(self):
+class TestNormalItem:
+    def test_name(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
-        assert "fixme" == items[0].name
+        assert "foo" == items[0].name
+
+    def test_sellin(self):
+        items = [Item("foo", 1, 0)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert 0 == items[0].sell_in
+
+    def test_sellin_negative(self):
+        items = [Item("foo", -1, 0)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert -2 == items[0].sell_in
+
+    def test_quality_decrease(self):
+        items = [Item("foo", 1, 2)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert 1 == items[0].quality
+
+    def test_quality_decrease_fast(self):
+        items = [Item("foo", -1, 4)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert 2 == items[0].quality
+
+    def test_quality_not_below_zero(self):
+        items = [Item("foo", -2, 1)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert 0 == items[0].quality
+
+    def test_quality_not_below_zero2(self):
+        items = [Item("foo", 2, 0)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert 0 == items[0].quality
+
+
+class TestSulfuras:
+    def test_name(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 0)]
+        gilded_rose = GildedRose(items)
+
+        gilded_rose.update_quality()
+
+        assert "Sulfuras, Hand of Ragnaros" == items[0].name
--- python/texttest_fixture.py
+++ python/texttest_fixture.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 from gilded_rose import *
 
 if __name__ == "__main__":
-    print ("OMGHAI!")
     items = [
              Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
              Item(name="Aged Brie", sell_in=2, quality=0),
@@ -13,6 +12,7 @@ if __name__ == "__main__":
              Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
              Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
              Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+             # Conjured item not implemented yet
              Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
             ]
 

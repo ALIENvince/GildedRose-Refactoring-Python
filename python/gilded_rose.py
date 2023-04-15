7fa6e902853035ad52de8d051290747cc2d72f36
ALIENvince <vince.bfay@gmail.com>
2023-04-15 18:40:59 +0200

    implemented all update methods

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -3,35 +3,53 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def update_normal(self, item):
+        item.sell_in -= 1
+
+        if item.quality == 0:
+            return
+
+        item.quality -= 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 1
+
+    def update_brie(self, item):
+        item.sell_in -= 1
+
+        if item.quality == 50:
+            return
+
+        item.quality += 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+    def update_sulfuras(self, item):
+        pass
+
+    def update_backstage(self, item):
+        item.sell_in -= 1
+
+        if item.sell_in < 0:
+            item.quality = 0
+            return
+
+        if item.sell_in < 5 and item.quality < 50:
+            item.quality += 1
+        if item.sell_in < 10 and item.quality < 50:
+            item.quality += 1
+        if item.sell_in >= 0 and item.quality < 50:
+            item.quality += 1
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.name == "foo":
+                return self.update_normal(item)
+            if item.name == "Aged Brie":
+                return self.update_brie(item)
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                return self.update_sulfuras(item)
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                return self.update_backstage(item)
 
 
 class Item:
